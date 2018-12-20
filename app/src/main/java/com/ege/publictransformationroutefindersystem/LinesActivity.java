0005bde7d55fabba9a5f84b8d13f5306fc9c9269
RumeysaTurker <rumeysatur@hotmail.com>
2018-12-20 15:00:37 +0300

    initial commit

--- app/src/main/java/com/ege/publictransformationroutefindersystem/LinesActivity.java
+++ app/src/main/java/com/ege/publictransformationroutefindersystem/LinesActivity.java
@@ -1,68 +1,77 @@
 package com.ege.publictransformationroutefindersystem;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 
 
 public class LinesActivity extends AppCompatActivity {
     private ListView listView;
-private TextView textView;
-private FirebaseDatabase database;
-private DatabaseReference myRef;
-private ArrayList<String> mVehicle=new ArrayList<>();
-Vehicles vehicle;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+    private ArrayList<String> mVehicle = new ArrayList<>();
+    private ArrayList<String> mKeys = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lines);
-      database=FirebaseDatabase.getInstance();
-      myRef=database.getReference("Vehicles");
+        database = FirebaseDatabase.getInstance();
+        myRef = database.getReference("Vehicles");
         listView = (ListView) findViewById(R.id.lwLines);
-          final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mVehicle);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mVehicle);
 
         listView.setAdapter(arrayAdapter);
-      myRef.addChildEventListener(new ChildEventListener() {
-          @Override
-          public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-              String value=dataSnapshot.getValue(String.class);
-              mVehicle.add(value);
-              arrayAdapter.notifyDataSetChanged();
-          }
+        myRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String value = dataSnapshot.getValue(String.class);
+                mVehicle.add(value);
+                String key=dataSnapshot.getKey();
+                mKeys.add(key);
+                arrayAdapter.notifyDataSetChanged();
+            }
 
-          @Override
-          public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                String value = dataSnapshot.getValue(String.class);
+                String key = dataSnapshot.getKey();
+                int index = mKeys.indexOf(key);
+                mVehicle.set(index, value);
+                arrayAdapter.notifyDataSetChanged();
+            }
 
-          }
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                mVehicle.remove(value);
+                arrayAdapter.notifyDataSetChanged();
+            }
 
-          @Override
-          public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-              String value=dataSnapshot.getValue(String.class);
-              mVehicle.remove(value);
-              arrayAdapter.notifyDataSetChanged();
-          }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
 
-          @Override
-          public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+            }
 
-          }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-          @Override
-          public void onCancelled(@NonNull DatabaseError databaseError) {
-
-          }
+            }
 
 
-      });
+        });
     }
 }
--- app/src/main/java/com/ege/publictransformationroutefindersystem/SettingsActivity.java
+++ app/src/main/java/com/ege/publictransformationroutefindersystem/SettingsActivity.java
@@ -2,6 +2,9 @@ package com.ege.publictransformationroutefindersystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -9,5 +12,34 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        EditText chsCity= (EditText) findViewById(R.id.chsCity);
+        chsCity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SettingsActivity.this, "Choose City Settings", Toast.LENGTH_SHORT).show();
+            }
+        });
+        EditText notif= (EditText) findViewById(R.id.notif);
+        notif.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SettingsActivity.this, "Notification Settings", Toast.LENGTH_SHORT).show();
+            }
+        });
+        EditText map= (EditText) findViewById(R.id.map);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SettingsActivity.this, "Map Settings", Toast.LENGTH_SHORT).show();
+            }
+        });
+        EditText privacy= (EditText) findViewById(R.id.privacy);
+        privacy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(SettingsActivity.this, "Privacy Settings", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
